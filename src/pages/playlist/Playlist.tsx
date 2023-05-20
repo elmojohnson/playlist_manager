e8aa59a40f30c4e9b33105494936e6baf16d13f6
John Elmo Johnson <elmoj6@gmail.com>
2023-05-20 16:27:22 +1000

    Updated Playlist component

--- src/pages/playlist/Playlist.tsx
+++ src/pages/playlist/Playlist.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Layout from "../../layout/app/Layout";
 
 const Playlist = () => {
   return (
     <Layout>
-      <h1>Playlist</h1>
       <Outlet />
     </Layout>
   );
