b1261d8bab903bc4155a1eb9c7082313516e4a42
John Elmo Johnson <elmoj6@gmail.com>
2023-05-20 16:26:32 +1000

    Playlist item UI

--- src/components/items/PlaylistItem.tsx
+++ src/components/items/PlaylistItem.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Playlist } from "../../types";
 
 const PlaylistItem: FC<Playlist> = (playlist) => {
-  return <Link to={"/playlist/" + playlist.id}>{playlist.name}</Link>;
+  const { id } = useParams();
+
+  return (
+    <Link
+      className={`px-4 py-1 rounded ${playlist.id === id && "bg-gray-300"}`}
+      to={"/playlist/" + playlist.id}
+    >
+      {playlist.name}
+    </Link>
+  );
 };
 
 export default PlaylistItem;
